27b71510c7dc3477d4a64cdfbabceb2266797dc4
Jason Boerner <jboerner56@gamil.com>
2019-05-02 15:27:46 -0400

    chatform file, form for submitting

--- src/Chatform.js
+++ src/Chatform.js
@@ -1,3 +1,19 @@
 import React from 'react';
 
+function ChatForm({text, handleChange, handleSend}) {
+    return (
+        <form
+        onSubmit={handleSend}
+        >
+            <input
+                value={text}
+                onChange={handleChange}
+            />
+            <button
+            onClick={handleSend}
+            >Submit</button>
+        </form>
+    );
+}
+
 export default Chatform;
